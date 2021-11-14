837aa84ce489ac9bf0c9dbb865f50c0ba6b843f0
Michaelo0 <nadnedmad@gmail.com>
2021-11-14 23:31:52 +0300

    Add files via upload

---
+++ src/main/kotlin/com/example/demo/BookService.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+
+@Service
+class BookService {
+    @Autowired
+    private val bookRepository: BookRepository? = null
+    fun list(): List<Book> {
+        return bookRepository!!.findAll()
+    }
+}
